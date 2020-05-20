ee65a4bfc4f3589a8803c5c20c485a496d98c4ac
qwnim2 <b05705009@ntu.edu.tw>
2020-05-20 15:42:52 +0800

    dqn

--- agent_dir/agent_dqn.py
+++ agent_dir/agent_dqn.py
@@ -114,6 +114,7 @@ class AgentDQN(Agent):
                 print(self.online_net(state).max(1)[1].view(1, 1))
                 return self.online_net(state).max(1)[1].view(1, 1)
         else:
+            print("OKOKOK===========")
             return torch.tensor([[random.randrange(self.num_actions)]], device= torch.device("cuda:0" if use_cuda else "cpu")
                                 , dtype=torch.long)
 
