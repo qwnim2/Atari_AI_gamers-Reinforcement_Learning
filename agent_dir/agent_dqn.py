bea63772c469bac912d4aa681edf7c28e965ab70
qwnim2 <b05705009@ntu.edu.tw>
2020-05-21 23:48:07 +0800

    dqn

--- agent_dir/agent_dqn.py
+++ agent_dir/agent_dqn.py
@@ -112,7 +112,7 @@ class AgentDQN(Agent):
                 return self.online_net(state).max(1)[1].view(1, 1)
         else:
             print("=======else======")
-            return random.randrange(self.action_size)
+            return random.randrange(self.num_actions)
             #return torch.tensor([[random.randrange(self.num_actions)]], dtype=torch.long).cuda()
             
     def update(self):
